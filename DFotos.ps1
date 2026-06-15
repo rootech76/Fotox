7cdc4c6b384ecdaa6865b2eeebb7a59e32625421
RooTech <rootech76@gmail.com>
2026-06-15 20:59:40 -0300

    Comparación de Hashes

--- DFotos.ps1
+++ DFotos.ps1
@@ -1,11 +1,31 @@
-#Elimina fotos duplicadas
+#Procesamiento de fotos duplicadas
 
 $Fotos = Get-ChildItem -Path 'R:\Midia\Fotos' -Recurse -file
 $Diccionario = @{}
+
+
+
+# Almacenando Rutas y Hashes en el diccionario
+
 for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
 
 	Write-Progress -Activity "Analizando Fotos" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
 	$Diccionario[$Fotos[$i].FullName] = (Get-FileHash $Fotos[$i] -Algorithm MD5).hash
+
+
+}
+
+
+
+for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
+
+	for ($c = $i + 1 ; $c -lt $Fotos.Count ; $c++){
+
+		if ($Diccionario[$Fotos[$i].FullName] -eq $Diccionario[$Fotos[$c].FullName]){
+
+		}
+
+	}
 
 
 }
