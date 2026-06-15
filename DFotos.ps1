11a9f10d54c6d9243ff8e062e8dcaa04c5baaf01
RooTech <rootech76@gmail.com>
2026-06-15 09:25:20 -0300

    Carga de las rutas y hash en el diccionario

--- DFotos.ps1
+++ DFotos.ps1
@@ -1,8 +1,11 @@
 #Elimina fotos duplicadas
 
 $Fotos = Get-ChildItem -Path 'R:\Midia\Fotos' -Recurse -file
-
+$Diccionario = @{}
 for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
+
 	Write-Progress -Activity "Analizando Fotos" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
+	$Diccionario[$Fotos[$i].FullName] = (Get-FileHash $Fotos[$i] -Algorithm MD5).hash
+
 
 }
