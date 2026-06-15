28ee9b421c82b699735803329dd42e0863c8bb76
RooTech <rootech76@gmail.com>
2026-06-14 22:44:22 -0300

    Agg el nivel de progreso

--- DFotos.ps1
+++ DFotos.ps1
@@ -1,3 +1,8 @@
 #Elimina fotos duplicadas
 
 $Fotos = Get-ChildItem -Path 'R:\Midia\Fotos' -Recurse -file
+
+for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
+	Write-Progress -Activity "Analizando Fotos" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
+
+}
