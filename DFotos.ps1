b5f811fe059702184941171b558d2fa5f69d70d6
RooTech <rootech76@gmail.com>
2026-06-15 22:15:42 -0300

    Comparar y Mover fotos duplucadas a cuarentena

--- DFotos.ps1
+++ DFotos.ps1
@@ -2,10 +2,11 @@
 
 $Fotos = Get-ChildItem -Path 'R:\Midia\Fotos' -Recurse -file | Where-Object { $_.FullName -notlike '*CUARENTENA*' }
 $Diccionario = @{}
+$RutaCUARENTENA = 'R:\Midia\Fotos\CUARENTENA'
 
-if (-not (Test-Path R:\Midia\Fotos\CUARENTENA)){
+if (-not (Test-Path $RutaCUARENTENA)){
 
-	New-Item -ItemType Directory -Path  R:\Midia\Fotos -Name CUARENTENA 
+	New-Item -ItemType Directory -Path $RutaCUARENTENA   
 
 }
 
@@ -15,21 +16,49 @@ if (-not (Test-Path R:\Midia\Fotos\CUARENTENA)){
 
 for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
 
-	Write-Progress -Activity "Analizando Fotos" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
+	Write-Progress -Activity "Cargando el Directorio" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
 	$Diccionario[$Fotos[$i].FullName] = (Get-FileHash $Fotos[$i] -Algorithm MD5).hash
 
 
 }
 
 
+#Comparar y Mover fotos duplucadas a cuarentena
 
 for ($i = 0 ; $i -lt $Fotos.Count ; $i++){
+
+	Write-Progress -Activity "Comparando Fotos" -Status "$($i+1) de $($Fotos.Count)" -PercentComplete (($i / $Fotos.Count) * 100)
+
+	$ContCoincidencias = 0
+	$HuboCoincidencias = $False
+
 
 	for ($c = $i + 1 ; $c -lt $Fotos.Count ; $c++){
 
 		if ($Diccionario[$Fotos[$i].FullName] -eq $Diccionario[$Fotos[$c].FullName]){
 
+			$ContCoincidencias++
+			$HuboCoincidencias = $True
+
+			$Destino = Join-Path -Path $RutaCUARENTENA  -ChildPath "$($i)-$($ContCoincidencias)$($Fotos[$i].Extension)"
+
+
+			Move-Item -Path $Fotos[$c].FullName -Destination $Destino
+
+
+
+
+
 		}
+
+	}
+
+	if ($HuboCoincidencias){
+
+
+		$Destino = Join-Path -Path $RutaCUARENTENA  -ChildPath "$($i)$($Fotos[$i].Extension)"
+		Move-Item -Path $Fotos[$i].FullName -Destination $Destino
+
 
 	}
 
