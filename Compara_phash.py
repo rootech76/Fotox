57fccbce5122c40bca6b016b3e79aa70c24e3448
RooTech <rootech76@gmail.com>
2026-06-17 18:29:14 -0300

    script para compara phash

---
+++ Compara_phash.py
@@ -0,0 +1,16 @@
+import sys
+from PIL import Image
+import imagehash
+
+
+ruta1 = sys.argv[1]
+ruta2 = sys.argv[2]
+
+
+
+hash1 = imagehash.phash(Image.open(ruta1))
+hash2 = imagehash.phash(Image.open(ruta2))
+
+diferencia = hash1 - hash2
+
+print(diferencia)
